c86e3bf9c1d497baefc58b7d469733194dfdfa64
Chad <chadbaily27@gmail.com>
2018-03-22 18:57:07 -0400

    base of 4.2

---
+++ 4.2.py
@@ -0,0 +1,27 @@
+
+
+x = 1000
+a = 7893
+c = 3517
+K = 2**13
+
+uVal = []
+count = 0
+itterations = 10000
+while count < itterations:
+
+    x = (a * x + c) % K
+    uVal.append(x / K)
+
+    count = count + 1
+
+sum = 0
+u = 7
+output = 0
+for i in uVal:
+    sum = sum + i
+    if sum >= x:
+        output = uVal.index(i)
+        break
+
+print(output)
