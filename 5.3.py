42aaede7dd9780ee33ae172604ff5437d83c74ea
Chad <chadbaily27@gmail.com>
2018-03-27 18:34:19 -0400

    Hopefully Finished 5.3

--- 5.3.py
+++ 5.3.py
@@ -1,19 +1,19 @@
 import math
 
 
-def callSim(uVal, count):
+def callSim(uVal, count, time):
     count = count + 1
     if count < 5:
         temp = uVal.pop()
         if temp > 0 and temp <= 0.2:
-            return callSim(uVal, count)
+            return callSim(uVal, count, time + 4)
         elif temp > 0.2 and temp <= 0.5:
-            return callSim(uVal, count)
+            return callSim(uVal, count, time + 26)
         else:
-            return -math.log(1 - temp) / 12
+            return time + ((-math.log(1 - temp) / 12) * 25)
             # return 12 * math.exp(-12 * temp)
     else:
-        return 'Unanswered'
+        return time
 
 
 x = 1000
@@ -37,7 +37,7 @@ responses = []
 #     print(i)
 
 for n in range(0, 500):
-    responses.append(callSim(uVal, 0))
+    responses.append(callSim(uVal, 0, 6))
 
 for response in responses:
     print(response)
